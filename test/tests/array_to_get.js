5502613d5b20fb7841dd3a3a025676e4171f586f
Lukas Liesis <liesislukas@gmail.com>
2016-09-21 15:05:15 +0300

    Create array_to_get.js

---
+++ test/tests/array_to_get.js
@@ -0,0 +1,18 @@
+import {describe, before, it} from 'mocha';
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const assert = chai.assert;
+
+import {array_to_get} from './../../../src/server/helpers/array_to_get';
+
+describe('server/helpers/array_to_get', function () {
+
+  it('array_to_get', () => {
+    let input = [];
+    input.push({var: 'a', val: '1'});
+    input.push({var: 'b', val: '2'});
+    input.push({var: 'c', val: '3'});
+    assert.equal('a=1&b=2&c=3', array_to_get({array: input}));
+  });
+
+});
